0b4f53313892ea101091058d81493766c1ccfb57
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-03-10 07:48:35 +0530

    Rename LatestTransaction to ClaimsTable

--- Frontend/src/pages/Dashboard/index.svelte
+++ Frontend/src/pages/Dashboard/index.svelte
@@ -3,39 +3,18 @@
     Container,
     Row,
     Col,
-    Button,
-   
-    Modal,
-    Input,
   } from "sveltestrap";
-
-
-  //import Charts
-
-  // Pages Components
  
-  import LatestTranaction from "./LatestTranaction.svelte";
-
-  //Import Breadcrumb
-  import Breadcrumbs from "../../common/Breadcrumb.svelte";
-
-  let subscribemodal = false;
-
-  const togglesubscribemodal = () => (subscribemodal = !subscribemodal);
-
-  
-
+  import ClaimsTable from "./ClaimsTable.svelte";
   
 </script>
 
 <div class="page-content">
   
   <Container fluid>
-    <Breadcrumbs title="Dashboards" breadcrumbItem="Dashboard" />
-   
     <Row>
       <Col lg="12">
-        <LatestTranaction />
+        <ClaimsTable />
       </Col>
     </Row>
   </Container>
