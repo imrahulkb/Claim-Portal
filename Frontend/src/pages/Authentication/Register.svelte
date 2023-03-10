1017be8f9d323705f626d1f93e8b537f33da19b6
LokeshC2 <47241840+LokeshC2@users.noreply.github.com>
2023-03-10 09:18:17 +0530

    fixed some routing

--- Frontend/src/pages/Authentication/Register.svelte
+++ Frontend/src/pages/Authentication/Register.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { auth } from '../../helpers/firebase';
 	import { navigate } from 'svelte-routing';
-	import { user } from '../../helpers/store.ts';
+	import { user } from '../../helpers/store';
     import { Container, Row, Col, Card, CardBody,Button} from 'sveltestrap';
     import { Link } from 'svelte-routing';
 
@@ -19,7 +19,7 @@
 			
 			user.set({ ...$user, loggedIn: true, email });
 			
-			navigate('/');
+			navigate('/dashboard');
 		}
 	};
 
--- Frontend/src/routes/index.ts
+++ Frontend/src/routes/index.ts
@@ -1,41 +1,8 @@
 import ClaimPortal from "../pages/ClaimPortal/index.svelte"
 
-// Pages Calendar
-import Calendar from "../pages/Calendar/index.svelte";
-
-//Tasks
-import TasksList from "../pages/Tasks/tasks-list.svelte";
-import TasksCreate from "../pages/Tasks/tasks-create.svelte";
-
-//Projects
-import ProjectsGrid from "../pages/Projects/projects-grid.svelte";
-import ProjectsList from "../pages/Projects/projects-list.svelte";
-import ProjectsOverview from "../pages/Projects/ProjectOverview/projects-overview.svelte";
-import ProjectsCreate from "../pages/Projects/projects-create.svelte";
-
-//Ecommerce Pages
-import EcommerceProducts from "../pages/Ecommerce/EcommerceProducts/index.svelte";
-import EcommerceProductDetail from "../pages/Ecommerce/EcommerceProducts/EcommerceProductDetail.svelte";
-import EcommerceOrders from "../pages/Ecommerce/EcommerceOrders/index.svelte";
-import EcommerceCustomers from "../pages/Ecommerce/EcommerceCustomers/index.svelte";
-import EcommerceCart from "../pages/Ecommerce/EcommerceCart.svelte";
-import EcommerceCheckout from "../pages/Ecommerce/EcommerceCheckout.svelte";
-import EcommerceShops from "../pages/Ecommerce/EcommerceShops/index.svelte";
-import EcommerceAddProduct from "../pages/Ecommerce/EcommerceAddProduct.svelte";
-
-//Email
-import EmailInbox from "../pages/Email/email-inbox.svelte";
-import EmailRead from "../pages/Email/email-read.svelte";
-import EmailBasicTemplte from "../pages/Email/email-basic-templte.svelte";
-import EmailAlertTemplte from "../pages/Email/email-template-alert.svelte";
-import EmailTemplateBilling from "../pages/Email/email-template-billing.svelte";
-
-//Invoices
-import InvoicesList from "../pages/Invoices/invoices-list.svelte";
-import InvoiceDetail from "../pages/Invoices/invoices-detail.svelte";
-
 // Authentication related pages
 import Login from "../pages/Authentication/Login.svelte";
+import Register from "../pages/Authentication/Register.svelte";
 
 // Inner Authentication
 import Login1 from "../pages/AuthenticationInner/Login.svelte";
@@ -57,114 +24,31 @@ import TwostepVerification2 from "../pages/AuthenticationInner/auth-two-step-ver
 
 // Dashboard
 import Dashboard from "../pages/Dashboard/index.svelte";
-import DashboardSaas from "../pages/Dashboard-saas/index.svelte";
-import DashboardCrypto from "../pages/Dashboard-crypto/index.svelte";
-import DashboardBlog from "../pages/Dashboard-blog/index.svelte";
-import DashboardJob from "../pages/DashboardJob/index.svelte"
-
-//Crypto
-import CryptoWallet from "../pages/Crypto/CryptoWallet/crypto-wallet.svelte";
-import CryptoBuySell from "../pages/Crypto/crypto-buy-sell.svelte";
-import CryptoExchange from "../pages/Crypto/crypto-exchange.svelte";
-import CryptoLending from "../pages/Crypto/crypto-lending.svelte";
-import CryptoOrders from "../pages/Crypto/CryptoOrders/crypto-orders.svelte";
-import CryptoKYCApplication from "../pages/Crypto/crypto-kyc-application.svelte";
-import CryptoIcoLanding from "../pages/Crypto/CryptoIcoLanding/index.svelte";
-
-// Charts
-import ChartApex from "../pages/Charts/Apexcharts.svelte";
-import ChartjsChart from "../pages/Charts/ChartjsChart.svelte";
-
-//Icons
-import IconBoxicons from "../pages/Icons/IconBoxicons.svelte";
-import IconDripicons from "../pages/Icons/IconDripicons.svelte";
-import IconMaterialdesign from "../pages/Icons/IconMaterialdesign.svelte";
-import IconFontawesome from "../pages/Icons/IconFontawesome.svelte";
-
-//Tables
-import BasicTables from "../pages/Tables/BasicTables.svelte";
-import DataTables from "../pages/Tables/DataTables.svelte";
-import EditableTables from "../pages/Tables/EditableTables.svelte";
-
-// Forms
-import FormElements from "../pages/Forms/FormElements/index.svelte";
-import FormLayouts from "../pages/Forms/FormLayouts.svelte";
-import FormAdvanced from "../pages/Forms/FormAdvanced.svelte";
-import FormEditors from "../pages/Forms/FormEditors.svelte";
-import FormRepeater from "../pages/Forms/FormRepeater.svelte";
-import FormUpload from "../pages/Forms/FormUpload.svelte";
-import FormWizard from "../pages/Forms/FormWizard.svelte";
-
-//Ui
-import UiAlert from "../pages/Ui/UiAlert.svelte";
-import UiButtons from "../pages/Ui/UiButtons.svelte";
-import UiCards from "../pages/Ui/UiCards.svelte";
-import UiCarousel from "../pages/Ui/UiCarousel.svelte";
-import UiColors from "../pages/Ui/UiColors.svelte";
-import UiDropdown from "../pages/Ui/UiDropdown.svelte";
-import UiOffCanvas from "../pages/Ui/UiOffCanvas.svelte";
-import UiGeneral from "../pages/Ui/UiGeneral.svelte";
-import UiGrid from "../pages/Ui/UiGrid.svelte";
-import UiImages from "../pages/Ui/UiImages.svelte";
-import UiModal from "../pages/Ui/UiModal.svelte";
-import UiProgressbar from "../pages/Ui/UiProgressbar.svelte";
-import UiSweetAlert from "../pages/Ui/UiSweetAlert.svelte";
-import UiTabsAccordions from "../pages/Ui/UiTabsAccordions.svelte";
-import UiTypography from "../pages/Ui/UiTypography.svelte";
-import UiVideo from "../pages/Ui/UiVideo.svelte";
-import UiSessionTimeout from "../pages/Ui/UiSessionTimeout.svelte";
-import UiRating from "../pages/Ui/UiRating.svelte";
-import UiRangeSlider from "../pages/Ui/UiRangeSlider.svelte";
-import UiToast from "../pages/Ui/UiToast.svelte";
-import UiPlaceholders from "../pages/Ui/UiPlaceholders.svelte";
-import UiUtilities from "../pages/Ui/UiUtilities.svelte";
 
 //Pages
-import PagesStarter from "../pages/Utility/pages-starter.svelte";
 import PagesMaintenance from "../pages/Utility/pages-maintenance.svelte";
 import PagesComingsoon from "../pages/Utility/pages-comingsoon.svelte";
-import PagesTimeline from "../pages/Utility/pages-timeline.svelte";
-import PagesFaqs from "../pages/Utility/pages-faqs.svelte";
-import PagesPricing from "../pages/Utility/pages-pricing.svelte";
 import Pages404 from "../pages/Utility/pages-404.svelte";
 import Pages500 from "../pages/Utility/pages-500.svelte";
 
-//Contacts
-import ContactsGrid from "../pages/Contacts/contacts-grid.svelte";
-import ContactsList from "../pages/Contacts/ContactList/contacts-list.svelte";
-import ContactsProfile from "../pages/Contacts/ContactsProfile/contacts-profile.svelte";
-
-//Blog
-import BlogList from "../pages/Blog/BlogList/index.svelte";
-import BlogGrid from "../pages/Blog/BlogGrid/index.svelte";
-import BlogDetails from "../pages/Blog/BlogDetails.svelte";
-
-//Job
-import JobGrid from "../pages/JobPages/JobGrid.svelte"
-import JobDetails from "../pages/JobPages/JobDetail.svelte"
-import JobCategories from "../pages/JobPages/JobCategories.svelte"
-import JobList from "../pages/JobPages/jobList.svelte";
-import ApplyJobs from "../pages/JobPages/ApplyJobs.svelte";
-import CandidateList from "../pages/JobPages/CandidateList.svelte";
-import CandidateOverview from "../pages/JobPages/CandidateOverview.svelte";
-
 const authProtectedRoutes = [
+  { path: "/", component: Dashboard},
+  { path: "/index", component: Dashboard },
   { path: "/dashboard", component: Dashboard },
- 
 ];
 
 const publicRoutes = [
-  { path: "/login", component: Login },
   { path: "/", component: ClaimPortal },
   { path: "/index", component: ClaimPortal },
-
-  // { path: "/register", component: Register },
+  { path: "/new-claim", component: ClaimPortal },
+  
+  { path: "/login", component: Login },
+  { path: "/register", component: Register },
 
   { path: "/pages-maintenance", component: PagesMaintenance },
   { path: "/pages-comingsoon", component: PagesComingsoon },
   { path: "/pages-404", component: Pages404 },
   { path: "/pages-500", component: Pages500 },
-  // { path: "/crypto-ico-landing", component: CryptoIcoLanding },
 
   // Authentication Inner
   { path: "/auth-login", component: Login1 },
